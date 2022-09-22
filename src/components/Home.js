7bd7ce1ea02dd4acbb4ef590edb9056b607c4357
Jane2do <107877817+Jane2do@users.noreply.github.com>
2022-09-21 20:34:48 -0500

    view edition with setAttribute

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,21 @@ import { onNavigate } from '../main.js';
 
 export const Home = () => {
   const div = document.createElement('div');
-  const title = document.createElement('h2');
+  const image = document.createElement('img');
+  image.setAttribute('id', 'logoHome');
   const buttonSend = document.createElement('button');
+  buttonSend.setAttribute('id', 'btnSend');
   const inputPost = document.createElement('input');
-
+  inputPost.setAttribute('id', 'userInputPost');
+  
+  image.src = 'img/logoHome.png';
   buttonSend.textContent = 'Send';
-  title.textContent = 'Home Welcome Traveler';
+
   buttonSend.addEventListener('click', () => {
     onNavigate(console.log('The button is working'));
   });
 
-  div.append(title, buttonSend, inputPost);
+  div.append(image, buttonSend, inputPost);
 
   return div;
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,15 +3,22 @@ import { onNavigate } from '../main.js';
 export const Login = () => {
   const div = document.createElement('div');
   const image = document.createElement('img');
+  image.setAttribute('id', 'logo');
   const blueLine = document.createElement('img');
+  blueLine.setAttribute('id', 'blueLine');
   const buttonLogin = document.createElement('button');
+  buttonLogin.setAttribute('id', 'buttonLogin');
   const buttonGoogle = document.createElement('button');
+  buttonGoogle.setAttribute('id', 'buttonGoogle');
   const buttonSignup = document.createElement('button');
+  buttonSignup.setAttribute('id', 'buttonSignupLogin');
   const inputEmail = document.createElement('input');
+  inputEmail.setAttribute('id', 'inputEmailLogin');
   const inputPass = document.createElement('input');
+  inputPass.setAttribute('id', 'passWordLog');
 
   image.src = 'img/logo.png';
-  blueLine.src = 'img/blueline.png';
+  blueLine.src = 'img/blueline2.png';
   buttonLogin.textContent = 'Log in';
   buttonGoogle.textContent = 'Sign in with Google';
   buttonSignup.textContent = 'Sign Up';
